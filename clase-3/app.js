360093048f3a25fa30fdaaffb43a2910d5afe73f
ghandidev <aboultaif_ghandi@hotmail.com>
2023-10-18 20:17:30 -0400

    class-3 final

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,17 +1,37 @@
 const express = require('express') // require --> CommonJs
 const crypto = require('node:crypto')
-
+const cors = require('cors')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:3000',
+        'http://localhost:1234',
+        'http://192.168.0.135:8080'
+      ]
+      if (!origin || ACCEPTED_ORIGINS.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  })
+)
 app.disable('x-powered-by') // desahabilitar el header x-powered-by: Express
 
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
-  // const { query, search } = req.query
+  // const origin = req.header('origin')
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter((movie) =>
@@ -50,6 +70,23 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie) // Actualizar la caché del cliente
 })
 
+app.delete('/movies/:id', (req, res) => {
+  // const origin = req.header('origin')
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
 app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
 
@@ -71,6 +108,16 @@ app.patch('/movies/:id', (req, res) => {
 
   return res.json(updateMovie)
 })
+
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+//   res.send('ok')
+// })
 
 const PORT = process.env.PORT ?? 1234
 
